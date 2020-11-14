b06b49bacc9946b81e4cd1d71696fe4ff877f97e
Marcus Guimarães <marcusv308@icloud.com>
2020-11-14 15:19:08 -0300

    Implementação do Painel de Log e a Finalização do Projeto

--- projeto-01-monstro/app.js
+++ projeto-01-monstro/app.js
@@ -4,8 +4,7 @@ new Vue({
         running: false,
         playerLife: 100,
         monsterLife: 100,
-
-
+        logs: []
     },
     computed:{
         hasResult(){
@@ -13,25 +12,45 @@ new Vue({
         }
     },
     methods:{
-
+        startGame(){
+            this.running = true
+            this.playerLife = 100
+            this.monsterLife = 100
+            this.logs = []
+        },
+        getRandom(min, max){
+            const value = Math.random() * (max - min) + min
+            return Math.round(value)
+        },
+        attack(special){
+            this.hurt('monsterLife',5,10, special, 'Jogador', 'Monstro', 'player')
+            if(this.monsterLife > 0){
+                this.hurt('playerLife',7, 12, false, 'Monstro', 'Jogador', 'monster')
+            }
+        },
+        hurt(props, min, max, special, source, target, cls){
+            const plus = special ? 5 : 0
+            const hurt = this.getRandom(min + plus, max + plus)
+            this[props] = Math.max(this[props] - hurt, 0)
+            this.registerLogs(`${source} atingiu ${target} com ${hurt}.`, cls)
+        },
+        heal(min, max){
+            const heal = this.getRandom(min, max)
+            this.playerLife = Math.min(this.playerLife + heal, 100)
+            this.registerLogs(`Jogador ganhou força de ${heal}.`, 'player')
+        },
+        healAndHurt(){
+            this.heal(10,15)
+            this.hurt('playerLife', 7, 12, false, 'Monstro', 'Jogador', 'monster')
+        },
+        registerLogs(text, cls){
+            this.logs.unshift({ text, cls })
+        }
     },
     watch:{
-        
+        hasResult(value){
+            if (value)
+                this.running = false
+        }
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 })
